51714392d88712de0b209489872109aa6a74e87e
dali14 <dalijlasii@gmail.com>
2022-11-16 16:58:47 +0100

    add resource Js Components and router

--- blog-post/resources/views/layouts/app.blade.php
+++ blog-post/resources/views/layouts/app.blade.php
@@ -8,10 +8,11 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
