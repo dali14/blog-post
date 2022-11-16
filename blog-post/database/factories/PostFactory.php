46cce42959944fd85297688e6567cf1af30f00be
dali14 <dalijlasii@gmail.com>
2022-11-16 15:47:42 +0100

    add faker and seeders and migration

--- blog-post/database/factories/PostFactory.php
+++ blog-post/database/factories/PostFactory.php
@@ -14,6 +14,8 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
+            'title' =>$this->faker->name(),
+            'text' =>$this->faker->text(),
             //
         ];
     }
